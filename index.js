aaacc16f09ab04c24106f2a4ce9653f464a735ff
tom-avilius <aaarshdwivedi244475@gmail.com>
2023-03-09 22:58:24 +0530

    Added function to get the contents of any directory.

--- index.js
+++ index.js
@@ -3,8 +3,18 @@
 
 // importing app and BrowserWindow from electron.
 const { app, BrowserWindow } = require('electron');
+// importing the file-system module
+const fs = require('fs');
 
 // ----------------------END OF IMPORTS-------------------------
+
+
+// function to load all files inside a specified folder
+// it will return an object that will store the details of individual files inside the folder
+const importDirectory = (directoryPath = "") => {
+  
+  return fs.readdirSync(directoryPath);
+}
 
 
 
@@ -20,6 +30,8 @@ const createMainWidow = () => {
 
   // centering the main window
   mainWindow.center();
+
+  const downloadsDirectory = importDirectory('C:/users/aaars/Downloads');
 }
 
 
